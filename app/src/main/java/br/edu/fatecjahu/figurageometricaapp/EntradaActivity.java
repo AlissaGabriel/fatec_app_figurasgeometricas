1388b7fd188b4b8dc6acabf7f52f77adfabae80e
AlissaGabriel <alissagabriel14@gmail.com>
2025-12-08 15:35:25 -0300

    feat: add ResultadoActivity and implement geometric figure detection logic

--- app/src/main/java/br/edu/fatecjahu/figurageometricaapp/EntradaActivity.java
+++ app/src/main/java/br/edu/fatecjahu/figurageometricaapp/EntradaActivity.java
@@ -1,6 +1,11 @@
 package br.edu.fatecjahu.figurageometricaapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +14,11 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class EntradaActivity extends AppCompatActivity {
+
+    EditText edtLado1, edtLado2, edtLado3;
+    Button btnAnalisar;
+    TextView tvSemFigura;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +29,54 @@ public class EntradaActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        edtLado1 = findViewById(R.id.edtLado1);
+        edtLado2 = findViewById(R.id.edtLado2);
+        edtLado3 = findViewById(R.id.edtLado3);
+        btnAnalisar = findViewById(R.id.btnAnalisar);
+        tvSemFigura = findViewById(R.id.tvSemFigura);
+
+        tvSemFigura.setVisibility(View.GONE);
+
+        btnAnalisar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (edtLado1.getText().toString().isEmpty()){
+
+                    tvSemFigura.setVisibility(View.VISIBLE);
+                    tvSemFigura.setText("Não é Figura Geométrica!!!");
+                    return;
+                }
+
+                int lado1 = Integer.parseInt(edtLado1.getText().toString());
+
+                int lado2 = 0;
+                if (!edtLado2.getText().toString().isEmpty()) {
+                    lado2 = Integer.parseInt(edtLado2.getText().toString());
+                }
+
+                int lado3 = 0;
+                if (!edtLado3.getText().toString().isEmpty()) {
+                    lado3 = Integer.parseInt(edtLado3.getText().toString());
+                }
+
+                if (lado1 == 0 && lado2 == 0 && lado3 == 0) {
+                    tvSemFigura.setVisibility(View.VISIBLE);
+                    tvSemFigura.setText("Não é Figura Geométrica!!!");
+                    return;
+                }
+
+                tvSemFigura.setVisibility(View.GONE);
+
+                Intent intent = new Intent(EntradaActivity.this, ResultadoActivity.class);
+
+                intent.putExtra("lado1", lado1);
+                intent.putExtra("lado2", lado2);
+                intent.putExtra("lado3", lado3);
+
+                startActivity(intent);
+            }
         });
     }
 }
---
+++ app/src/main/java/br/edu/fatecjahu/figurageometricaapp/ResultadoActivity.java
@@ -0,0 +1,85 @@
+package br.edu.fatecjahu.figurageometricaapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ResultadoActivity extends AppCompatActivity {
+    TextView tvLado3, tvLado1Resultado, tvLado2Resultado, tvLado3Resultado, tvResultadoDaFigura, tvResultadoTriangulo;
+
+    private String descobrirFigura(int lado1, int lado2, int lado3) {
+        if (lado3 > 0) {
+            return "Triângulo";
+        }
+        if ((lado1 > 0 && lado2 == 0 && lado3 == 0) ||
+                (lado2 > 0 && lado1 == 0 && lado3 == 0)) {
+            return "Quadrado";
+        }
+        if (lado1 == lado2) {
+            return "Quadrado";
+        } else {
+            return "Retângulo";
+        }
+    }
+
+    private String descobrirTipoTriangulo(int lado1, int lado2, int lado3) {
+        if (lado1 == lado2 && lado2 == lado3) {
+            return "Triângulo Equilátero";
+        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
+            return "Triângulo Isósceles";
+        } else {
+            return "Triângulo Escaleno";
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_resultado);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        tvLado1Resultado = findViewById(R.id.tvLado1Resultado);
+        tvLado2Resultado = findViewById(R.id.tvLado2Resultado);
+        tvLado3Resultado = findViewById(R.id.tvLado3Resultado);
+        tvLado3 = findViewById(R.id.tvLado3);
+        tvResultadoDaFigura = findViewById(R.id.tvResultadoDaFigura);
+        tvResultadoTriangulo = findViewById(R.id.tvResultadoTriangulo);
+
+        int lado1 = getIntent().getIntExtra("lado1", 0);
+        int lado2 = getIntent().getIntExtra("lado2", 0);
+        int lado3 = getIntent().getIntExtra("lado3", 0);
+
+        tvLado1Resultado.setText(String.valueOf(lado1));
+        tvLado2Resultado.setText(String.valueOf(lado2));
+
+        String figura = descobrirFigura(lado1, lado2, lado3);
+        tvResultadoDaFigura.setText(figura);
+
+        if (figura.equals("Triângulo")) {
+            tvLado3.setVisibility(View.VISIBLE);
+            tvLado3Resultado.setVisibility(View.VISIBLE);
+            tvLado3Resultado.setText(String.valueOf(lado3));
+
+            String tipo = descobrirTipoTriangulo(lado1, lado2, lado3);
+            tvResultadoTriangulo.setText(tipo);
+        } else {
+            tvLado3Resultado.setVisibility(View.GONE);
+            tvLado3.setVisibility(View.GONE);
+        }
+
+        Button btnVoltar = findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(v -> finish());
+    }
+}
